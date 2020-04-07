c7d4893141b3e880cb5321ebb7252753c254f05c
Andrew.Wu <Andrew.Wu@neobards.com>
2020-04-07 17:45:59 +0800

    LARP Data Converter for Blue/Red

---
+++ LarpCreater/LARP BlueRed/LARP DataConverter/LARP Data Converter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LARP_Data_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
