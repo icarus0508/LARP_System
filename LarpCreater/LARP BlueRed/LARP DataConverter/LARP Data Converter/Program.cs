13e6bbfd059dcdc33fd2dc69d899561576ed1caf
Andrew.Wu <Andrew.Wu@neobards.com>
2020-04-08 16:50:31 +0800

    Data excel to json Converter

--- LarpCreater/LARP BlueRed/LARP DataConverter/LARP Data Converter/Program.cs
+++ LarpCreater/LARP BlueRed/LARP DataConverter/LARP Data Converter/Program.cs
@@ -1,12 +1,175 @@
 ﻿using System;
+using System.Data;
+using ExcelDataReader;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace LARP_Data_Converter
 {
     class Program
     {
-        static void Main(string[] args)
+        static public string AppPath;
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AppPath = AppDomain.CurrentDomain.BaseDirectory;
+            Console.WriteLine("Base Directory: " + AppPath);
+
+            LoadSkillExcelToJson();
+            LoadClassExcelToJson();
+            return 0;
+        }
+
+        public static void SaveJason(string path, string tjson)
+        {
+            string FinalPath = AppPath + "\\ExportJson\\" + path;
+            Console.WriteLine("Save Jason To: " + FinalPath);
+            StreamWriter sw = File.CreateText(FinalPath);          
+            sw.Close();
+
+            File.WriteAllText(FinalPath, tjson);
+        }
+        
+        public static DataSet LoadExcel(string ExcelFilePath)
+        {
+            DataSet ds;
+
+            string file = AppPath+"\\Excel\\"+ExcelFilePath;
+                if (File.Exists(file))
+                {
+                    var extension = Path.GetExtension(file).ToLower();
+                    Console.WriteLine("Load file：" + file);
+                    using (var stream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                    {
+                        IExcelDataReader reader = null;
+                        if (extension == ".xls")
+                        {
+                            Console.WriteLine(" => XLS");
+                            reader = ExcelReaderFactory.CreateBinaryReader(stream, new ExcelReaderConfiguration()
+                            {
+                                FallbackEncoding = Encoding.GetEncoding("big5")
+                            });
+                        }
+                        else if (extension == ".xlsx")
+                        {
+                            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+                            Console.WriteLine(" => XLSX");
+                            reader = ExcelReaderFactory.CreateOpenXmlReader(stream, new ExcelReaderConfiguration()
+                            {
+                                FallbackEncoding = Encoding.GetEncoding("big5")
+                            });
+                        }
+                        else if (extension == ".csv")
+                        {
+                            Console.WriteLine(" => CSV");
+                            reader = ExcelReaderFactory.CreateCsvReader(stream, new ExcelReaderConfiguration()
+                            {
+                                FallbackEncoding = Encoding.GetEncoding("big5")
+                            });
+                        }
+                        else if (extension == ".txt")
+                        {
+                            Console.WriteLine(" => Text(Tab Separated)");
+                            reader = ExcelReaderFactory.CreateCsvReader(stream, new ExcelReaderConfiguration()
+                            {
+                                FallbackEncoding = Encoding.GetEncoding("big5"),
+                                AutodetectSeparators = new char[] { '\t' }
+                            });
+                        }
+
+                        if (reader == null)
+                        {
+                            Console.WriteLine("Unknown file extension：" + extension);
+                            Console.ReadKey();
+                            return null ;
+                        }
+                        Console.WriteLine(" => progressing");
+                        using (reader)
+                        {
+
+                            ds = reader.AsDataSet(new ExcelDataSetConfiguration()
+                            {
+                                UseColumnDataType = false,
+                                ConfigureDataTable = (tableReader) => new ExcelDataTableConfiguration()
+                                {
+                                    UseHeaderRow = false
+                                }
+                            });
+
+
+                        }
+                    }
+
+                    return ds;
+                }
+                else
+                {
+                Console.WriteLine("File "+ ExcelFilePath+" Not exist");
+                return null ;
+                }
+
+            
+        }
+        public static void LoadSkillExcelToJson()
+        {
+             DataSet ds ;
+            ds = LoadExcel("Skill.xlsx");
+            if (ds == null) return; 
+
+            var table = ds.Tables[0];
+            Skill_Save_Info_List tSSIL = new Skill_Save_Info_List();
+
+            for (int i = 1; i < table.Rows.Count; i++)
+            {
+                Skill_Save_Info tSkillInfo = new Skill_Save_Info();
+                tSkillInfo.Name = table.Rows[i][0].ToString();
+                tSkillInfo.ImageName = table.Rows[i][1].ToString();
+                tSkillInfo.Description = table.Rows[i][2].ToString();
+                tSkillInfo.DetailDescription = table.Rows[i][3].ToString();
+                tSkillInfo.AvaClass = table.Rows[i][4].ToString();
+                tSkillInfo.PreSkillName = table.Rows[i][5].ToString();
+                tSkillInfo.HPBuff = table.Rows[i][6].ToString();
+                tSkillInfo.MPBuff = table.Rows[i][7].ToString();
+                tSkillInfo.ArrowBuff = table.Rows[i][8].ToString();
+                tSkillInfo.ThrowBuff = table.Rows[i][9].ToString();
+                tSkillInfo.AvaRank = table.Rows[i][10].ToString();
+                tSkillInfo.MaxCost = table.Rows[i][11].ToString();
+                tSkillInfo.AvaSide = table.Rows[i][12].ToString();
+                tSSIL.SkillList.Add(tSkillInfo);
+            }
+
+            string json = JsonConvert.SerializeObject(tSSIL, Formatting.Indented);
+
+            SaveJason("SkillListJason.json", json);
+        }
+        public static void LoadClassExcelToJson()
+        {
+            DataSet ds ;
+            ds= LoadExcel("ClassesBase.xlsx");
+            if (ds == null) return;
+
+            var table = ds.Tables[0];
+            Class_Save_Info_List tCSIL = new Class_Save_Info_List();
+
+            for (int i = 1; i < table.Rows.Count; i++)
+            {
+                Class_Save_Info tclassInfo = new Class_Save_Info();
+                tclassInfo.className = table.Rows[i][0].ToString();
+                tclassInfo.rankName = table.Rows[i][1].ToString();
+                tclassInfo.baseHP = table.Rows[i][2].ToString();
+                tclassInfo.baseSkillPoint = table.Rows[i][3].ToString();
+                tclassInfo.baseMP = table.Rows[i][4].ToString();
+                tclassInfo.baseArrows = table.Rows[i][5].ToString();
+                tclassInfo.baseThrowing = table.Rows[i][6].ToString();
+                tclassInfo.baseSurperSkillPoint = table.Rows[i][7].ToString();
+                tCSIL.classList.Add(tclassInfo);
+            }
+
+            string json = JsonConvert.SerializeObject(tCSIL, Formatting.Indented);
+
+            SaveJason("ClassesListJason.json", json);
         }
     }
 }
---
+++ LarpCreater/LARP BlueRed/LARP DataConverter/LARP Data Converter/SupportDataClasses.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LARP_Data_Converter
+{
+    public class Class_Save_Info
+    {
+        public string className { get; set; }
+        public string rankName { get; set; }
+        public string baseHP { get; set; }
+        public string baseSkillPoint { get; set; }
+        public string baseSurperSkillPoint { get; set; }
+        public string baseArrows { get; set; }
+        public string baseMP { get; set; }
+        public string baseThrowing { get; set; }
+    }
+    public class Class_Save_Info_List
+    {
+        public List<Class_Save_Info> classList = new List<Class_Save_Info>();
+
+    }
+
+    public class Skill_Save_Info
+    {
+        public string Name { get; set; }
+        public string ImageName { get; set; }
+        public string Description { get; set; }
+        public string DetailDescription { get; set; }
+        public string AvaClass { get; set; }
+        public string PreSkillName { get; set; }
+        public string HPBuff { get; set; }
+        public string MPBuff { get; set; }
+        public string ArrowBuff { get; set; }
+        public string ThrowBuff { get; set; }
+        public string AvaRank { get; set; }
+        public string MaxCost { get; set; }
+        public string AvaSide { get; set; }
+    }
+
+    public class Skill_Save_Info_List
+    {
+        public List<Skill_Save_Info> SkillList = new List<Skill_Save_Info>();
+
+    }
+}
